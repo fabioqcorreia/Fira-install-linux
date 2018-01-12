0686909ade308afe0b259d69c9554f7e39f03497
fabioqcorreia <fabioqcorreia@gmail.com>
2018-01-11 21:26:10 -0300

    adicionada verificação de existência do curl e unzip, especificado que o script funciona em distro ubuntu e adicionado campo de lingua no readme, tutorial será incluído.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Fira-install-linux
-This is a simple bash script inspired by other script that installs from the mozilla's github the fira font in linux distributions
+
+## Tutorial PT-BR
+
+Este é um script simples inspirado pelo script da Mozilla que instala a fonte Fira nas distribuições baseadas em Ubuntu.
+
+> Adicionarei mais scripts de instalação para outras distribuições linux,
+> Planejo adicionar scripts para o CentOS e Fedora no futuro.
+
+## Tutorial EN
+
+This is a simple bash script inspired by other script made by Mozilla that installs the fira font for Ubuntu distros.
+
+> I will add more install scripts for other linux distros,
+> I plan to add CentOS and Fedora in the future.
--- fira-install.sh
+++ fira-install.sh
@@ -1,17 +1,43 @@
 !#	/bin/sh
 
-wget https://github.com/carrois/Fira/archive/master.zip
-unzip master.zip
+# Verifica se o curl está instalado e instala caso não estiver...
+echo "Verificando dependências..."
 
-sudo mkdir -p /usr/share/fonts/opentype/fira_code
-sudo mkdir -p /usr/share/fonts/opentype/fira_mono
-sudo mkdir -p /usr/share/fonts/opentype/fira_sans
+if ! which curl
+then
+    apt-get update -qq
+    apt-get install curl
+fi
 
-sudo cp master/Fira_Code_3_2/Fonts/FiraCode_OTF_32/* /usr/share/fonts/opentype/fira_code
-sudo cp master/Fira_Mono_3_2/Fonts/FiraMono_OTF_32/* /usr/share/fonts/opentype/fira_mono
-sudo cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Roman/* /usr/share/fonts/opentype/fira_sans
-sudo cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Italic/* /usr/share/fonts/opentype/fira_sans
-sudo cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Roman/* /usr/share/fonts/opentype/fira_sans
-sudo cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Italic/* /usr/share/fonts/opentype/fira_sans
-sudo cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Roman/* /usr/share/fonts/opentype/fira_sans
-sudo cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Italic/* /usr/share/fonts/opentype/fira_sans
+if ! which unzip
+then
+    apt-get update -qq
+    apt-get install unzip
+fi
+
+echo "Baixando o pacote de fontes..."
+
+# Faz o download do arquivo da fonte direta dos criadores da fonte Fira.
+curl https://github.com/carrois/Fira/archive/master.zip -o /home/$USER/master.zip -s
+
+# Verifica se o Unzip está instalado...
+echo "Instalando as fontes..."
+unzip /home/$USER/master.zip
+
+mkdir -p /usr/share/fonts/opentype/fira_code
+mkdir -p /usr/share/fonts/opentype/fira_mono
+mkdir -p /usr/share/fonts/opentype/fira_sans
+
+cp master/Fira_Code_3_2/Fonts/FiraCode_OTF_32/* /usr/share/fonts/opentype/fira_code
+cp master/Fira_Mono_3_2/Fonts/FiraMono_OTF_32/* /usr/share/fonts/opentype/fira_mono
+cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Roman/* /usr/share/fonts/opentype/fira_sans
+cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Italic/* /usr/share/fonts/opentype/fira_sans
+cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Roman/* /usr/share/fonts/opentype/fira_sans
+cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Italic/* /usr/share/fonts/opentype/fira_sans
+cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Roman/* /usr/share/fonts/opentype/fira_sans
+cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Italic/* /usr/share/fonts/opentype/fira_sans
+
+echo "Limpando a sujeira..."
+rm -rf /home/$USER/master.zip
+
+echo "Feito!"
