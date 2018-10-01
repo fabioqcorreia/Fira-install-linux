3b4ab8e7ded8f195d65e151ca465bf7c72adf4e9
fabioqcorreia <fabioqcorreia@gmail.com>
2018-10-01 17:14:09 -0300

    Update fira-install.sh

--- fira-install.sh
+++ fira-install.sh
@@ -28,16 +28,17 @@ mkdir -p /usr/share/fonts/opentype/fira_code
 mkdir -p /usr/share/fonts/opentype/fira_mono
 mkdir -p /usr/share/fonts/opentype/fira_sans
 
-cp master/Fira_Code_3_2/Fonts/FiraCode_OTF_32/* /usr/share/fonts/opentype/fira_code
-cp master/Fira_Mono_3_2/Fonts/FiraMono_OTF_32/* /usr/share/fonts/opentype/fira_mono
-cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Roman/* /usr/share/fonts/opentype/fira_sans
-cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Italic/* /usr/share/fonts/opentype/fira_sans
-cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Roman/* /usr/share/fonts/opentype/fira_sans
-cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Italic/* /usr/share/fonts/opentype/fira_sans
-cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Roman/* /usr/share/fonts/opentype/fira_sans
-cp master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Italic/* /usr/share/fonts/opentype/fira_sans
+cp /home/$USER/master/Fira_Code_3_2/Fonts/FiraCode_OTF_32/* /usr/share/fonts/opentype/fira_code
+cp /home/$USER/master/Fira_Mono_3_2/Fonts/FiraMono_OTF_32/* /usr/share/fonts/opentype/fira_mono
+cp /home/$USER/master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Roman/* /usr/share/fonts/opentype/fira_sans
+cp /home/$USER/master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Compressed/Italic/* /usr/share/fonts/opentype/fira_sans
+cp /home/$USER/master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Roman/* /usr/share/fonts/opentype/fira_sans
+cp /home/$USER/master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Condensed/Italic/* /usr/share/fonts/opentype/fira_sans
+cp /home/$USER/master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Roman/* /usr/share/fonts/opentype/fira_sans
+cp /home/$USER/master/Fira_Sans_4_2/Fonts/FiraSans_OTF_4203/Normal/Italic/* /usr/share/fonts/opentype/fira_sans
 
 echo "Limpando a sujeira..."
-rm -rf /home/$USER/master.zip
+rm /home/$USER/master.zip
+rm -r /home/$USER/master
 
 echo "Feito!"
